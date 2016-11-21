a52b62141dfe79eb662f4685301e6655c5e1bfc0
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2016-11-22 00:44:15 +0100

    minify bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
+	entry: "./playground/main.js",
+	output: {
+		path: 'playground/dist',
+		filename: "bundle.js"
+	},
 	node: {
 		fs: "empty"
 	},
+	devtool: "source-map",
 	plugins: [
 		new BabiliPlugin()
 	]
